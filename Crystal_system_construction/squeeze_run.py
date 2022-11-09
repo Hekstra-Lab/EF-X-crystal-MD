9708a8f346a4743ce979bc7891186b568c8b76d3
Ziyuan Zhao <ziyuanzhao@college.harvard.edu>
2022-11-09 08:20:46 -0500

    Added scripts for doing the volume equilibration on HPC platform

---
+++ Crystal_system_construction/squeeze_run.py
@@ -0,0 +1,30 @@
+# import stuff
+from openmm.app import PDBFile, ForceField
+from openff.toolkit.topology import Molecule
+from openmmforcefields.generators import GAFFTemplateGenerator
+from simtk.unit import *
+import mdtraj
+import mdtools
+import pickle 
+
+
+# initialize forcefield
+# need to have openmmforcefields installed first
+forcefield = ForceField('amber/ff14SB.xml', 
+                        'amber/tip3p_standard.xml',
+                        'amber/tip3p_HFE_multivalent.xml',
+                        '../CRO_parametrization/cro.xml')
+acetate = Molecule.from_smiles('CC(=O)[O-]')
+gaff = GAFFTemplateGenerator(molecules=acetate)
+forcefield.registerTemplateGenerator(gaff.generator)
+
+# load the crystal system
+crystal = PDBFile("neutralized.pdb")
+mdsystem = mdtools.LatticeMDSystem(crystal.topology,
+                                   crystal.positions, 
+                                   forcefield, "P 21 21 21")
+
+mdsystem.buildSimulation()
+print("System initialized for squeezing. Currently running on platform ", mdsystem.simulation.context.getPlatform().getName())
+mdsystem.squeeze(tolerance=0.0005, maxIterations=20, dt=0.002*picoseconds, initial_water_perturb=1160)
+mdsystem.save("squeezed.pdb")
---
+++ Crystal_system_construction/squeeze_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH -t 0-08:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p gpu
+#SBATCH --mem=64G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o squeeze_gpu_%A_%a.out
+#SBATCH -e squeeze_gpu_%A_%a.err
+
+source $LOGIN/.bashrc
+source $LOGIN/.bash_addons 
+conda activate openmm
+PENMM_DEFAULT_PLATFORM=CUDA
+python squeeze_run.py
+
